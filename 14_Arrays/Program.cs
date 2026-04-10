6ac07af80ad86d531ff5d9de306ffe868b7a9bce
Hanabusec <58026496+Hanabusec@users.noreply.github.com>
2026-04-10 12:45:20 +0300

    Add files via upload

---
+++ 14_Arrays/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Collections - Koleksiyonlar
+
+//ARRAYS - Static - Fixed Size
+
+
+//LİSTS - Dynamic - Dynamic Size
+
+//Creating An array of player names
+
+string[] players = new string[4];
+players[0] = "Mustafa";
+players[1] = "Ali";
+players[2] = "Miray";
+players[3] = "Kerem Miraç";
+
+//players[4] = "Sinem";
+
+//for (int i = 0; i < players.Length; i++)
+//{
+//    Console.WriteLine(players[i]);
+//}
+
+//string[] playerClasses = new string[] { "Warrior", "Paladin", "Rogue", "Wizard" };
+
+//for (int i = 0; i < playerClasses.Length; i++)
+//{
+//    Console.WriteLine(playerClasses[i]);
+//}
+
+//Console.WriteLine("Please enter the size of aaray");
+//string[] names = new string[int.Parse(Console.ReadLine())];
+
+//for (int i = 0; i < names.Length; i++)
+//{
+//    Console.WriteLine("Enter the names for index: " + i);
+//    names[i] = Console.ReadLine();
+//    Console.WriteLine(names[i]);
+//}
+
+char[] word = new char[] { 'H', 'e', 'l', 'l', 'o' };
+
+foreach (char harf in word)
+{
+    Console.Write(harf);
+}
+
+//do
+//{
+    
+//}
+//while (true);
+
+Console.ReadKey();
+
+
+
---
+++ 15_Lists/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+List<string> names = new List<string>();
+
+//List<char> word = new List<char>() 
+//{ 
+//    'a', 'b', 'c'
+//};
+
+//foreach (char item in word)
+//{
+//    Console.WriteLine(item);
+//}
+
+names.Add("Miray");
+names.Add("Mustafa");
+names.Add("Ali");
+names.Add("Kerem Miraç");
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+//names.Remove(names[2]);
+//names.Remove("Kerem Miraç");
+
+//for (int i = 0; i < names.Count; i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+//names.RemoveAt(0);
+//names.Clear();
+
+names.IndexOf("Ali");
+names.AddRange(new string[] { "a", "b", "c" });
+
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+if(names.Contains("Aliii"))
+{
+    Console.WriteLine("Ali is here");
+}
+
+Console.ReadKey();
