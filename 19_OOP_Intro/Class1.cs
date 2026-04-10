471457d2bf50b18e08ca2682d4e158e996eb3f2a
Hanabusec <58026496+Hanabusec@users.noreply.github.com>
2026-04-10 12:40:53 +0300

    Add files via upload

---
+++ 19_OOP_Intro/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_OOP_Intro
+{
+    internal class Class1
+    {
+    }
+}
---
+++ 19_OOP_Intro/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine();
+//Console.WriteLine();
+
+//Object oriented programming is based on the concept of objects containing data and methods..
+
+//DateTime
+
+//List<string> list = new List<string>();
+
+//var pizzaDay = new DateTime(2025, 4, 3);
+
+//Console.WriteLine(pizzaDay.Year);
+//Console.WriteLine(pizzaDay.Month);
+//Console.WriteLine(pizzaDay.Day);
+//Console.WriteLine(pizzaDay.DayOfWeek);
+
+//Console.WriteLine($"The next pizza day will be on " + pizzaDay.AddYears(1));
+
+//Console.ReadKey();
+
+//Abstraction
+//Only essential data and information is given to the user
+//Abstraction means that classes expose only essential data and methods and hide the underlying details..
+
+//Classes
+//How to define a class
+//What the fields of a class are
+//What the default values of fields are
+//What a default constructor is
+
+//Fields
+//Property
+//Methods
+//Construct
+
+var rectangle1 = new Rectangle(7,12);
+
+//Console.WriteLine(rectangle1.width);
+//Console.WriteLine(rectangle1.height);
+
+var rectangle2 = new Rectangle(6,78);
+Console.WriteLine("Area of rectangle2 is: " + rectangle2.CalculateArea());
+rectangle2.CalculatePerimeter();
+//var rectangle3 = new Rectangle();
+
+Console.ReadKey();
+
+class Rectangle
+{
+    //Alan ve çevrfe hesabı yapmak..
+
+    //Fields - Class değişkenleri.
+    //All the attributes class will have.
+    int width;
+    int height;
+
+    //Construct
+    public Rectangle(int width, int height)
+    {
+       //_width = width;
+       //_height = height;
+
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(float width, float height)
+    {
+        
+    }
+
+    //Methods
+    public int CalculateArea()
+    {
+        return width * height;
+    }
+
+    public void CalculatePerimeter()
+    {
+        int perimeter = 2 * (width + height);
+        Console.WriteLine("Perimeter of the rectangle is: " + perimeter);
+    }
+   
+}
+
+class Square
+{
+
+}
+
+class Triangle
+{
+
+}
+
